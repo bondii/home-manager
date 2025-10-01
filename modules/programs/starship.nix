262d77d80f899142191de8625dfd9f97f40de061
Pontus Eriksson <pontus.eriksson@opac.se>
2025-10-01 10:15:17 +0200

    Add Python venv to Starship prompt

--- modules/programs/starship.nix
+++ modules/programs/starship.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       add_newline = false;
-      format = "$username$hostname$directory$git_branch$git_status$cmd_duration$line_break$character";
+      format = "$username$hostname$directory$virtualenv$git_branch$git_status$cmd_duration$line_break$character";
       character = {
         success_symbol = "❯";
         error_symbol = "❯";
