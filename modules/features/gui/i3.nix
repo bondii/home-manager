db3d7ede4e7e0f07b3010e2ba5553cc226997cf1
Pontus Eriksson <pontus.eriksson@opac.se>
2025-10-02 11:46:53 +0200

    Make volume adjustments finer

--- modules/features/gui/i3.nix
+++ modules/features/gui/i3.nix
@@ -215,8 +215,8 @@ in
 
             "${M}+r" = ''mode "resize"'';
 
-            "XF86AudioRaiseVolume" = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +10%";
-            "XF86AudioLowerVolume" = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -10%";
+            "XF86AudioRaiseVolume" = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +5%";
+            "XF86AudioLowerVolume" = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -5%";
             "XF86AudioMute" = "exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle";
             "XF86AudioMicMute" = "exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle";
 
